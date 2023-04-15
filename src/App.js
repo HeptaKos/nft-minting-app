f996e78351be6963478fa17e10a0d31f390b95e9
HeptaKos <45622031+HeptaKos@users.noreply.github.com>
2023-04-15 16:58:02 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -206,6 +206,8 @@ function App() {
         </a>
         <s.SpacerSmall />
         <ResponsiveWrapper flex={1} style={{ padding: 24 }} test>
+          <s.Container flex={1} jc={"center"} ai={"center"}>
+          </s.Container>
           <s.SpacerLarge />
           <s.Container
             flex={2}
@@ -395,6 +397,8 @@ function App() {
             <s.SpacerMedium />
           </s.Container>
           <s.SpacerLarge />
+          <s.Container flex={1} jc={"center"} ai={"center"}>
+          </s.Container>
         </ResponsiveWrapper>
         <s.SpacerMedium />
         <s.Container jc={"center"} ai={"center"} style={{ width: "70%" }}>
